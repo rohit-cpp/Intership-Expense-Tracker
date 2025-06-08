a8663d07afc90ad63de3a80fceb02ec9ca95db54
Rohit <rrgawanderohit@gmail.com>
2025-06-09 01:21:21 +0530

    changed vite file again

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,12 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Intership-Expense-Tracker",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
