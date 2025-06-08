d65e8ff6b170248ad0c3e8b1305254999072f943
Rohit <rrgawanderohit@gmail.com>
2025-06-09 01:03:39 +0530

    changed vite file

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,9 +6,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: process.env.VITE_BASE_PATH || "/Intership-Expense-Tracker",
 });
